84d02584a395019985829ab235826c2b5625a6d8
cstorm125 <charin.polpanumas@datatouille.org>
2019-10-01 06:14:44 +0000

    Add a script to train transformer with fairseq

--- run_fairseq.sh
+++ run_fairseq.sh
@@ -1,12 +1,13 @@
-# fairseq-preprocess --source-lang en --target-lang th \
-#     --trainpref data/opensubtitles_tok/train \
-#     --validpref data/opensubtitles_tok/valid \
-#     --testpref data/opensubtitles_tok/test \
-#     --destdir data/opensubtitles_bin
+fairseq-preprocess --source-lang en --target-lang th \
+    --trainpref data/opensubtitles_tok/train \
+    --validpref data/opensubtitles_tok/valid \
+    --testpref data/opensubtitles_tok/test \
+    --destdir data/opensubtitles_bin
 
 fairseq-train \
     data/opensubtitles_bin \
-    --arch transformer_iwslt_de_en --share-decoder-input-output-embed \
+    --arch transformer_iwslt_de_en --max-epoch 10 \
+    --share-decoder-input-output-embed \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
     --dropout 0.3 --weight-decay 0.0001 \
@@ -14,8 +15,10 @@ fairseq-train \
     --max-tokens 2048 \
     --bpe sentencepiece \
     --memory-efficient-fp16
-    --save-dir data/opensubtitles_model/transformers
     
 fairseq-generate data/opensubtitles_bin \
-    --path data/opensubtitles_model/transformers/checkpoint_best.pt \
-    --remove-bpe --beam 5 
+    --path checkpoints/checkpoint_best.pt \
+    --remove-bpe --beam 5 --max-tokens 2048
+
+# | Translated 328154 sentences (2773243 tokens) in 1517.1s (216.31 sentences/s, 1828.01 tokens/s)
+# | Generate test with beam=5: BLEU4 = 10.80, 36.6/15.3/7.2/3.4 (BP=1.000, ratio=1.029, syslen=2445089, reflen=2376306)
