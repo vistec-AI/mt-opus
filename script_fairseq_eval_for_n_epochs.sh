d64fd02414f38f7b23fef18e022406daf1936875
cstorm125 <charin.polpanumas@datatouille.org>
2019-10-01 06:16:35 +0000

    Add a script to evaluate BLEU score from epoch i to j

---
+++ script_fairseq_eval_for_n_epochs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+start=$1
+end=$2
+step=$3
+data_path=$4
+checkpoint_prefix=$5
+beam=$6
+max_tokens=$7
+opts=$8
+
+for (( i=$start; i<=$end; i+=$step ))
+do
+  echo "Evaluate BLEU at epoch number $i of the model checkpoint: $checkpoint_prefix/checkpoint$i.pt";
+  echo "beam_size=$beam, max_tokens=$max_tokens";
+
+  fairseq-generate $data_path \
+	--path ${checkpoint_prefix}/checkpoint${i}.pt \
+	--quiet \
+	--beam $beam \
+	--max-tokens $max_tokens $opts > ${checkpoint_prefix}/result_checkpoint_${i}.txt
+
+  echo "Done evaluation for epoch $i";
+
+  
+done
