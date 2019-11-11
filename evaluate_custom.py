89c90351fac1061028e441e6aea2117f9c2977ab
Saiko Yoneyabashi <artificialcc@gmail.com>
2019-11-11 17:36:52 +0700

    Refactor code (cuda device)

--- evaluate_custom.py
+++ evaluate_custom.py
@@ -250,41 +250,42 @@ def evaluate(epoch,
     # 2. Optimize ensemble for generation
     device = torch.device("cuda:" + str(parser_args.gpu) if use_cuda else "cpu")
     print("device:", device)
-    
+
     #set device
-    torch.cuda.device(device)
-    for model in models:
-        model.make_generation_fast_(
-            beamable_mm_beam_size=None if parser_args.beam == None else parser_args.beam,
-            need_attn=False,
-        )
-        if use_cuda:
-            model.cuda()
+    with torch.cuda.device(parser_args.gpu):
 
-    # print(models)
-    # 3. Acquite Batch iterator
-    print("INFO: Acquire Batch iterator")
-    _iterator = get_batch_iterator(dataset, parser_args, epoch=epoch, seed=1, max_positions=None)
-    # print("epoch_iter", iterator)
-    # print('length of vocab size,', len(tgt_dict))
+        for model in models:
+            model.make_generation_fast_(
+                beamable_mm_beam_size=None if parser_args.beam == None else parser_args.beam,
+                need_attn=False,
+            )
+            if use_cuda:
+                model.cuda()
 
-    # print('tgt vocab last 10 sumbols', tgt_dict[-1])
-    _generator = build_generator(tgt_dict=tgt_dict, args=parser_args)
+        # print(models)
+        # 3. Acquite Batch iterator
+        print("INFO: Acquire Batch iterator")
+        _iterator = get_batch_iterator(dataset, parser_args, epoch=epoch, seed=1, max_positions=None)
+        # print("epoch_iter", iterator)
+        # print('length of vocab size,', len(tgt_dict))
 
-
-    # 5. Initiate scorer
-    # scorer = bleu.Scorer(tgt_dict.pad(), tgt_dict.eos(), tgt_dict.unk())
-    _scorer = bleu.Scorer(tgt_dict_newmm.pad(), tgt_dict_newmm.eos(), tgt_dict_newmm.unk())
+        # print('tgt vocab last 10 sumbols', tgt_dict[-1])
+        _generator = build_generator(tgt_dict=tgt_dict, args=parser_args)
 
 
-    scorer, list_translation_results = _evaluate(parser_args=parser_args,
-                                                 models=models,
-                                                 iterator=_iterator,
-                                                 generator=_generator,
-                                                 scorer=_scorer,
-                                                 use_cuda=use_cuda)
+        # 5. Initiate scorer
+        # scorer = bleu.Scorer(tgt_dict.pad(), tgt_dict.eos(), tgt_dict.unk())
+        _scorer = bleu.Scorer(tgt_dict_newmm.pad(), tgt_dict_newmm.eos(), tgt_dict_newmm.unk())
 
-    return scorer, list_translation_results
+
+        scorer, list_translation_results = _evaluate(parser_args=parser_args,
+                                                    models=models,
+                                                    iterator=_iterator,
+                                                    generator=_generator,
+                                                    scorer=_scorer,
+                                                    use_cuda=use_cuda)
+
+        return scorer, list_translation_results
 
 DATASET_NAMES = ["wang"]
 if __name__ == '__main__':
